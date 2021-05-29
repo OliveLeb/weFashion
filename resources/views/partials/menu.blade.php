3438044dd4068c314eb2d80483ae3eb0ecac7987
OliveLeb <lebel.olivier@wanadoo.fr>
2021-05-29 19:42:46 +0200

    admin menu + create CategoryController

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,22 +1,42 @@
 <nav class="navbar navbar-expand-md navbar-light bg-light">
-  <a class="navbar-brand logo" href="{{route('home')}}">We Fashion</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-        {{-- @if ($is_discounted) --}}
-            <li class="nav-item">
-                <a class="nav-link" href="{{route('discount.products')}}">Solde</a>
-            </li>
-        {{-- @endif --}}
+    @if(Route::is('admin.*'))
+        <span class="navbar-brand logo">We Fashion</span>
+    @else
+        <a class="navbar-brand logo" href="{{route('home')}}">We Fashion</a>
+    @endif
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-        @foreach ($categories as $id=>$category)
-            <li class="nav-item">
-                <a class="nav-link" href="{{route('category.products',$id)}}">{{$category}}</a>
-            </li>
-        @endforeach
-    </ul>
+
+        <ul class="navbar-nav mr-auto">
+            @if(Route::is('admin.*'))
+
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route('admin.products.index')}}">Produits</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Catégories</a>
+                </li>
+
+            @else
+                {{-- @if ($is_discounted) --}}
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('discount.products')}}">Solde</a>
+                    </li>
+                {{-- @endif --}}
+
+                @foreach ($categories as $id=>$category)
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('category.products',$id)}}">{{$category}}</a>
+                    </li>
+                @endforeach
+
+            @endif
+        </ul>
+
+
 
     <ul class="navbar-nav ml-auto">
         @guest
@@ -36,17 +56,22 @@
                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                     {{ Auth::user()->name }}
                 </a>
-
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <ul>
-                        <li>
-                            <a href="{{route('admin.products.index')}}" class="dropdown-item">Dashboard</a>
-                        </li>
+                {{-- <div class="dropdown-menu" aria-labelledby="navbarDropdown"> --}}
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        @if(Route::is('admin.*'))
+                            <li>
+                                <a class="dropdown-item" href="{{route('home')}}"><i class="fas fa-tshirt go-to"></i> Home</a>
+                            </li>
+                         @else
+                            <li>
+                                <a href="{{route('admin.products.index')}}" class="dropdown-item"><i class="fas fa-table go-to"></i> Dashboard</a>
+                            </li>
+                        @endif
                         <li>
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
                                                 document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
+                                <i class="fas fa-sign-out-alt log-out"></i> {{ __('Logout') }}
                             </a>
 
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
@@ -54,7 +79,7 @@
                             </form>
                         </li>
                     </ul>
-                </div>
+                {{-- </div> --}}
             </li>
         @endguest
     </ul>
